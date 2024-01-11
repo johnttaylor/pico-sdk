2391fb74fd47678da5323c42dc66d01eb94feb4a
johnt <john.t.taylor@gmail.com>
2024-01-11 18:31:04 -0500

    fix header includes

--- src/common/pico_base/include/pico/config_autogen.h
+++ src/common/pico_base/include/pico/config_autogen.h
@@ -4,8 +4,8 @@
 
 // based on PICO_CONFIG_HEADER_FILES:
 
-#include "C:/_workspaces/raspberry/pico-sdk/src/boards/include/boards/pico.h"
+#include "pico-sdk/src/boards/include/boards/pico.h"
 
 // based on PICO_RP2040_CONFIG_HEADER_FILES:
 
-#include "C:/_workspaces/raspberry/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h"
+#include "pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h"
